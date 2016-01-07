14a3d072a91f5559967f9198bdcd285a99bc65ea
Bhagwat Kumar <bhagwat@intelligrape.com>
2016-01-08 03:36:48 +0530

    docker destination dir

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,22 +2,30 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
-	}
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
 
+    @RequestMapping("/{name}/**")
+    public String hello(@PathVariable("name") String name, HttpServletRequest request) {
+        String restOfTheUrl = (String) request.getAttribute(
+                HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        return "Hello " + name + "! You provided : " + restOfTheUrl;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
